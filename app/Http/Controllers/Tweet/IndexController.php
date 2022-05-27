e521533851baf5dbab6dbbeee944a51e056f0957
rs0122 <rs.0122.ssthe2@hotmail.co.jp>
2022-05-27 17:26:38 +0900

    [bugfix]一部コメントアウト

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -17,8 +17,9 @@ class IndexController extends Controller
     public function __invoke(Request $request)
     {
         $tweets = Tweet::orderBy('created_at', 'DESC')->get();
-        $tweets = Tweet::all()->sortByDesc('created_at');
-        // Laravel独自のヘルパー関数。(dump,dieの頭文字)
+        // orderByとall()->sortByDesc()はSQL時のソートかPHPコードでのソートかで大きく違う。SQLでソートする方が高速。
+        // $tweets = Tweet::all()->sortByDesc('created_at');
+        // ↓Laravel独自のヘルパー関数。(dump,dieの頭文字)
         // dd($tweets);
         // return view('tweet.index', ['name' => 'laravel']);
         // 第二引数に指定する以外にもwith関数を使用する方法もある。
